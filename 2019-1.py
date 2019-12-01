945cfb4572f072d1b49758c179d4a9e78b5bfe85
SharpBit <31069084+SharpBit@users.noreply.github.com>
2019-12-01 11:41:19 -0500

    day 1

---
+++ 2019-1.py
@@ -0,0 +1,23 @@
+from utils import timed
+
+# https://adventofcode.com/2019/day/1
+
+@timed
+def part_one():
+    print(sum(int(mass) // 3 - 2 for mass in open('2019-1.txt').readlines()))
+
+
+def fuel_for_mass(mass):
+    required_fuel = mass // 3 - 2
+    if required_fuel <= 0:
+        return 0
+    return required_fuel + fuel_for_mass(required_fuel)
+
+
+@timed
+def part_two():
+    print(sum(fuel_for_mass(int(mass)) for mass in open('2019-1.txt').readlines()))
+
+
+part_one()
+part_two()
