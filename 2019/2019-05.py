57544cc15f67e2cd36bfc1a77a0b000f6cc33622
SharpBit <31069084+SharpBit@users.noreply.github.com>
2019-12-08 13:06:31 -0500

    change day 5 intcode to a class

--- 2019/2019-05.py
+++ 2019/2019-05.py
@@ -1,77 +1,65 @@
 from utils import timed
-# Took almost 3 hours
 
-def solve_intcode(intcode, given_input):
-    opcode = None
-    pointer = 0
-    while True:
-        opcode = f'{intcode[pointer]:05}'
+class IntcodeSolver:
+    def __init__(self, intcode):
+        self.intcode = intcode.copy()
+        self.pointer = 0
 
-        if opcode[-2:] == '99':
-            break
-        if opcode[-2:] == '01':
-            save_index = intcode[pointer + 3]
-            first_num = intcode[pointer + 1] if opcode[-3] == '0' else pointer + 1
-            second_num = intcode[pointer + 2] if opcode[-4] == '0' else pointer + 2
-            intcode[save_index] = intcode[first_num] + intcode[second_num]
-            pointer += 4
-        elif opcode[-2:] == '02':
-            save_index = intcode[pointer + 3]
-            first_num = intcode[pointer + 1] if opcode[-3] == '0' else pointer + 1
-            second_num = intcode[pointer + 2] if opcode[-4] == '0' else pointer + 2
-            intcode[save_index] = intcode[first_num] * intcode[second_num]
-            pointer += 4
-        elif opcode[-2:] == '03':
-            save_index = intcode[pointer + 1]
-            intcode[save_index] = given_input
-            pointer += 2
-        elif opcode[-2:] == '04':
-            index = intcode[pointer + 1] if opcode[-3] == '0' else pointer + 1
-            output = intcode[index]
-            print(output)
-            pointer += 2
-        elif opcode[-2:] == '05':
-            check_index = intcode[pointer + 1] if opcode[-3] == '0' else pointer + 1
-            set_index = intcode[pointer + 2] if opcode[-4] == '0' else pointer + 2
-            if intcode[check_index] != 0:
-                pointer = intcode[set_index]
+    def get_param(self, mode, n):
+        if mode == 0:
+            return self.intcode[self.intcode[self.pointer + n]]
+        elif mode == 1:
+            return self.intcode[self.pointer + n]
+
+    def run(self, given_input):
+        while True:
+            instruction = f'{self.intcode[self.pointer]:05}'
+            opcode = int(instruction[-2:])
+            modes = [int(m) for m in reversed(instruction[:3])]
+
+            if opcode == 1:
+                self.intcode[self.intcode[self.pointer + 3]] = self.get_param(modes[0], 1) + self.get_param(modes[1], 2)
+                self.pointer += 4
+            elif opcode == 2:
+                self.intcode[self.intcode[self.pointer + 3]] = self.get_param(modes[0], 1) * self.get_param(modes[1], 2)
+                self.pointer += 4
+            elif opcode == 3:
+                self.intcode[self.intcode[self.pointer + 1]] = given_input
+                self.pointer += 2
+            elif opcode == 4:
+                output = self.get_param(modes[0], 1)
+                print(output)
+                self.pointer += 2
+            elif opcode == 5:
+                if self.get_param(modes[0], 1) != 0:
+                    self.pointer = self.get_param(modes[1], 2)
+                else:
+                    self.pointer += 3
+            elif opcode == 6:
+                if self.get_param(modes[0], 1) == 0:
+                    self.pointer = self.get_param(modes[1], 2)
+                else:
+                    self.pointer += 3
+            elif opcode == 7:
+                self.intcode[self.intcode[self.pointer + 3]] = int(self.get_param(modes[0], 1) < self.get_param(modes[1], 2))
+                self.pointer += 4
+            elif opcode == 8:
+                self.intcode[self.intcode[self.pointer + 3]] = int(self.get_param(modes[0], 1) == self.get_param(modes[1], 2))
+                self.pointer += 4
+            elif opcode == 99:
+                break
             else:
-                pointer += 3
-        elif opcode[-2:] == '06':
-            check_index = intcode[pointer + 1] if opcode[-3] == '0' else pointer + 1
-            set_index = intcode[pointer + 2] if opcode[-4] == '0' else pointer + 2
-            if intcode[check_index] == 0:
-                pointer = intcode[set_index]
-            else:
-                pointer += 3
-        elif opcode[-2:] == '07':
-            first_index = intcode[pointer + 1] if opcode[-3] == '0' else pointer + 1
-            second_index = intcode[pointer + 2] if opcode[-4] == '0' else pointer + 2
-            if intcode[first_index] < intcode[second_index]:
-                intcode[intcode[pointer + 3]] = 1
-            else:
-                intcode[intcode[pointer + 3]] = 0
-            pointer += 4
-        elif opcode[-2:] == '08':
-            first_index = intcode[pointer + 1] if opcode[-3] == '0' else pointer + 1
-            second_index = intcode[pointer + 2] if opcode[-4] == '0' else pointer + 2
-            if intcode[first_index] == intcode[second_index]:
-                intcode[intcode[pointer + 3]] = 1
-            else:
-                intcode[intcode[pointer + 3]] = 0
-            pointer += 4
-        else:
-            print(f'Unknown opcode {opcode}')
+                print(f'Unknown opcode {opcode} at index {self.pointer}')
 
 @timed
 def part_one():
     intcode = [int(x) for x in open('inputs/2019-05.txt').read().split(',')]
-    solve_intcode(intcode, 1)
+    IntcodeSolver(intcode).run(1)
 
 @timed
 def part_two():
     intcode = [int(x) for x in open('inputs/2019-05.txt').read().split(',')]
-    solve_intcode(intcode, 5)
+    IntcodeSolver(intcode).run(5)
 
 
 part_one()
