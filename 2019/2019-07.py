418f40613dafced16e3a2fb0a3495c54343bee26
SharpBit <31069084+SharpBit@users.noreply.github.com>
2019-12-08 18:15:23 -0500

    day 7 intcode into class

--- 2019/2019-07.py
+++ 2019/2019-07.py
@@ -1,101 +1,79 @@
+import itertools
+
 from utils import timed
-from typing import List, Tuple
-# the explanation of order of inputs for part 2 took forever to understand
 
-def solve_intcode(intcode: List[int], input_values: List[int], pointer: int=0) -> Tuple[int, List[int], int, bool]:
-    input_index = 0
-    output = None
-    halted = False
 
-    opcode = None
-    while True:
-        opcode = f'{intcode[pointer]:05}'
+class IntcodeSolver:
+    def __init__(self, intcode):
+        self.intcode = intcode.copy()
+        self.pointer = 0
+        self.output = None
 
-        if opcode[-2:] == '99':
-            halted = True
-            break
-        if opcode[-2:] == '01':
-            save_index = intcode[pointer + 3]
-            first_num = intcode[pointer + 1] if opcode[-3] == '0' else pointer + 1
-            second_num = intcode[pointer + 2] if opcode[-4] == '0' else pointer + 2
-            intcode[save_index] = intcode[first_num] + intcode[second_num]
-            pointer += 4
-        elif opcode[-2:] == '02':
-            save_index = intcode[pointer + 3]
-            first_num = intcode[pointer + 1] if opcode[-3] == '0' else pointer + 1
-            second_num = intcode[pointer + 2] if opcode[-4] == '0' else pointer + 2
-            intcode[save_index] = intcode[first_num] * intcode[second_num]
-            pointer += 4
-        elif opcode[-2:] == '03':
-            save_index = intcode[pointer + 1]
-            try:
-                intcode[save_index] = input_values[input_index]
-            except IndexError:
-                return output, intcode, pointer, halted
-            input_index += 1
-            pointer += 2
-        elif opcode[-2:] == '04':
-            index = intcode[pointer + 1] if opcode[-3] == '0' else pointer + 1
-            output = intcode[index]
-            pointer += 2
-        elif opcode[-2:] == '05':
-            check_index = intcode[pointer + 1] if opcode[-3] == '0' else pointer + 1
-            set_index = intcode[pointer + 2] if opcode[-4] == '0' else pointer + 2
-            if intcode[check_index] != 0:
-                pointer = intcode[set_index]
-            else:
-                pointer += 3
-        elif opcode[-2:] == '06':
-            check_index = intcode[pointer + 1] if opcode[-3] == '0' else pointer + 1
-            set_index = intcode[pointer + 2] if opcode[-4] == '0' else pointer + 2
-            if intcode[check_index] == 0:
-                pointer = intcode[set_index]
-            else:
-                pointer += 3
-        elif opcode[-2:] == '07':
-            first_index = intcode[pointer + 1] if opcode[-3] == '0' else pointer + 1
-            second_index = intcode[pointer + 2] if opcode[-4] == '0' else pointer + 2
-            if intcode[first_index] < intcode[second_index]:
-                intcode[intcode[pointer + 3]] = 1
-            else:
-                intcode[intcode[pointer + 3]] = 0
-            pointer += 4
-        elif opcode[-2:] == '08':
-            first_index = intcode[pointer + 1] if opcode[-3] == '0' else pointer + 1
-            second_index = intcode[pointer + 2] if opcode[-4] == '0' else pointer + 2
-            if intcode[first_index] == intcode[second_index]:
-                intcode[intcode[pointer + 3]] = 1
-            else:
-                intcode[intcode[pointer + 3]] = 0
-            pointer += 4
-        else:
-            raise ValueError(f'Unknown opcode {opcode} in index {pointer}')
+    def get_param(self, mode, n):
+        if mode == 0:
+            return self.intcode[self.intcode[self.pointer + n]]
+        elif mode == 1:
+            return self.intcode[self.pointer + n]
 
-    return output, intcode, pointer, halted
+    def run(self, inputs):
+        """Returns output[int], is_running[bool]"""
+        # Part 2 only passes in 1 input
+        if type(inputs) == int:
+            inputs = [inputs]
+
+        while True:
+            instruction = f'{self.intcode[self.pointer]:05}'
+            opcode = int(instruction[-2:])
+            modes = [int(m) for m in reversed(instruction[:3])]
+
+            if opcode == 1:
+                self.intcode[self.intcode[self.pointer + 3]] = self.get_param(modes[0], 1) + self.get_param(modes[1], 2)
+                self.pointer += 4
+            elif opcode == 2:
+                self.intcode[self.intcode[self.pointer + 3]] = self.get_param(modes[0], 1) * self.get_param(modes[1], 2)
+                self.pointer += 4
+            elif opcode == 3:
+                try:
+                    self.intcode[self.intcode[self.pointer + 1]] = inputs.pop(0)
+                except IndexError:
+                    return self.output, True
+                self.pointer += 2
+            elif opcode == 4:
+                self.output = self.get_param(modes[0], 1)
+                self.pointer += 2
+            elif opcode == 5:
+                if self.get_param(modes[0], 1) != 0:
+                    self.pointer = self.get_param(modes[1], 2)
+                else:
+                    self.pointer += 3
+            elif opcode == 6:
+                if self.get_param(modes[0], 1) == 0:
+                    self.pointer = self.get_param(modes[1], 2)
+                else:
+                    self.pointer += 3
+            elif opcode == 7:
+                self.intcode[self.intcode[self.pointer + 3]] = int(self.get_param(modes[0], 1) < self.get_param(modes[1], 2))
+                self.pointer += 4
+            elif opcode == 8:
+                self.intcode[self.intcode[self.pointer + 3]] = int(self.get_param(modes[0], 1) == self.get_param(modes[1], 2))
+                self.pointer += 4
+            elif opcode == 99:
+                return self.output, False
+            else:
+                print(f'Unknown opcode {opcode} at index {self.pointer}')
+
 
 @timed
 def part_one():
     intcode = [int(x) for x in open('inputs/2019-07.txt').read().split(',')]
     combos = []
-    for a in range(5):
-        for b in range(5):
-            for c in range(5):
-                for d in range(5):
-                    for e in range(5):
-                        if len(set([a, b, c, d, e])) < 5:
-                            continue
-                        intcode_copy = intcode.copy()
-                        amp_a, *_ = solve_intcode(intcode_copy, [a, 0])
-                        intcode_copy = intcode.copy()
-                        amp_b, *_ = solve_intcode(intcode_copy, [b, amp_a])
-                        intcode_copy = intcode.copy()
-                        amp_c, *_ = solve_intcode(intcode_copy, [c, amp_b])
-                        intcode_copy = intcode.copy()
-                        amp_d, *_ = solve_intcode(intcode_copy, [d, amp_c])
-                        intcode_copy = intcode.copy()
-                        amp_e, *_ = solve_intcode(intcode_copy, [e, amp_d])
-
-                        combos.append(int(amp_e))
+    for p in itertools.permutations(range(5)):
+        output, _ = IntcodeSolver(intcode).run([p[0], 0])
+        output, _ = IntcodeSolver(intcode).run([p[1], output])
+        output, _ = IntcodeSolver(intcode).run([p[2], output])
+        output, _ = IntcodeSolver(intcode).run([p[3], output])
+        output, _ = IntcodeSolver(intcode).run([p[4], output])
+        combos.append(output)
 
     print(max(combos))
 
@@ -103,40 +81,19 @@ def part_one():
 def part_two():
     intcode = [int(x) for x in open('inputs/2019-07.txt').read().split(',')]
     combos = []
-    for a in range(5, 10):
-        for b in range(5, 10):
-            for c in range(5, 10):
-                for d in range(5, 10):
-                    for e in range(5, 10):
-                        if len(set([a, b, c, d, e])) < 5:
-                            continue
+    for p in itertools.permutations(range(5, 10)):
+        amps = []
+        last_output = 0
+        is_running = True
+        for i in range(5):
+            amps.append(IntcodeSolver(intcode))
+            amps[i].run(p[i])
 
-                        intcode_a = intcode.copy()
-                        intcode_b = intcode.copy()
-                        intcode_c = intcode.copy()
-                        intcode_d = intcode.copy()
-                        intcode_e = intcode.copy()
+        while is_running:
+            for i, a in enumerate(amps):
+                last_output, is_running = a.run(last_output)
 
-                        loop = 0
-                        halted = False
-                        while not halted:
-                            if loop == 0:
-                                amp_a, intcode_a, pointer_a, _ = solve_intcode(intcode_a, [a])
-                                amp_b, intcode_b, pointer_b, _ = solve_intcode(intcode_b, [b])
-                                amp_c, intcode_c, pointer_c, _ = solve_intcode(intcode_c, [c])
-                                amp_d, intcode_d, pointer_d, _ = solve_intcode(intcode_d, [d])
-                                amp_e, intcode_e, pointer_e, halted = solve_intcode(intcode_e, [e])
-                                amp_e = 0
-                            else:
-                                amp_a, intcode_a, pointer_a, _ = solve_intcode(intcode_a, [amp_e], pointer=pointer_a)
-                                amp_b, intcode_b, pointer_b, _ = solve_intcode(intcode_b, [amp_a], pointer=pointer_b)
-                                amp_c, intcode_c, pointer_c, _ = solve_intcode(intcode_c, [amp_b], pointer=pointer_c)
-                                amp_d, intcode_d, pointer_d, _ = solve_intcode(intcode_d, [amp_c], pointer=pointer_d)
-                                amp_e, intcode_e, pointer_e, halted = solve_intcode(intcode_e, [amp_d], pointer=pointer_e)
-
-                            loop += 1
-
-                        combos.append(int(amp_e))
+        combos.append(last_output)
 
     print(max(combos))
 
