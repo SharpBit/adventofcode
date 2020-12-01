1ecff2677ef50a8350231af1c44abaf20563fb4e
SharpBit <31069084+SharpBit@users.noreply.github.com>
2020-12-01 09:03:40 -0500

    fix part 1

--- 2020/2020-01.py
+++ 2020/2020-01.py
@@ -5,7 +5,7 @@ inputs = list(map(int, open('inputs/2020-01.txt').readlines()))
 @timed
 def part_one():
     for i, x in enumerate(inputs):
-        for y in inputs:
+        for y in inputs[i + 1:]:
             if x + y == 2020:
                 return x * y
 
