97bbeb1920671676eb3607b16daf2bc06e50e168
SharpBit <31069084+SharpBit@users.noreply.github.com>
2020-12-01 08:25:24 -0500

    2020 day 1!

---
+++ 2020/2020-01.py
@@ -0,0 +1,24 @@
+from utils import timed
+
+inputs = list(map(int, open('inputs/2020-01.txt').readlines()))
+
+@timed
+def part_one():
+    for i, x in enumerate(inputs):
+        for y in inputs:
+            if x + y == 2020:
+                return x * y
+
+
+print(part_one())
+
+@timed
+def part_two():
+    for i, x in enumerate(inputs):
+        for j, y in enumerate(inputs[i + 1:]):
+            for z in inputs[j + 1:]:
+                if x + y + z == 2020:
+                    return x * y * z
+
+
+print(part_two())
---
+++ 2020/utils.py
@@ -0,0 +1,12 @@
+from functools import wraps
+import time
+
+def timed(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        begin = time.time()
+        result = f(*args, **kwargs)
+        delta = time.time() - begin
+        print('Executed {} in {} sec'.format(f.__name__, delta))
+        return result
+    return wrapper
