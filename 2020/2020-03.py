24ca44a27d71a4ffdc2cfc0195e958f3d1ecb64b
SharpBit <31069084+SharpBit@users.noreply.github.com>
2020-12-03 13:08:37 -0500

    use modulo instead of multipling the whole list of strings loll

--- 2020/2020-03.py
+++ 2020/2020-03.py
@@ -4,15 +4,13 @@ with open('inputs/2020-03.txt') as f:
     rows = list(map(lambda r: r.strip(), f.readlines()))  # get rid of newline chars
 
 def find_num_trees(row_increment, col_increment):
-    for i, row in enumerate(rows):
-        rows[i] = row * int(len(rows) * col_increment / len(row) + 1)
     row = 0
     column = 0
     trees = 0
     while row < len(rows) - 1:
         row += row_increment
         column += col_increment
-        if rows[row][column] == '#':
+        if rows[row][column % len(rows[row])] == '#':
             trees += 1
 
     return trees
@@ -21,11 +19,16 @@ def find_num_trees(row_increment, col_increment):
 def part_one():
     return find_num_trees(1, 3)
 
-
 @timed
 def part_two():
     fnt = find_num_trees
-    return fnt(1, 1) * fnt(1, 3) * fnt(1, 5) * fnt(1, 7) * fnt(2, 1)
+    slopes = ((1, 1), (1, 3), (1, 5), (1, 7), (2, 1))
+
+    num_trees_list = [fnt(*slope) for slope in slopes]
+    product = 1
+    for result in num_trees_list:
+        product *= result
+    return product
 
 
 print(part_one())
