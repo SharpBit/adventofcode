8fe13f239fc6dc8b7a33deb7f59562f91a7ba68a
SharpBit <31069084+SharpBit@users.noreply.github.com>
2020-12-17 12:38:43 -0500

    finally finish day 17 with 2x less code

--- 2020/2020-17.py
+++ 2020/2020-17.py
@@ -5,51 +5,57 @@ from utils import timed
 with open('inputs/2020-17.txt') as f:
     z_layer_0 = [list(row) for row in f.read().splitlines()]
 
-def get_adjacent_cubes(cycle_layout, mutable_layout, x, y, z):
-    adjustments = set(itertools.combinations([-1, 0, 1] * 3, 3))
-    adjustments.remove((0, 0, 0))
-
-    adj_cubes = []
-    for x_adj, y_adj, z_adj in adjustments:
-        try:
-            adj_cubes.append(cycle_layout[(x + x_adj, y + y_adj, z + z_adj)])
-        except KeyError:
-            mutable_layout[(x + x_adj, y + y_adj, z + z_adj)] = '.'
-            adj_cubes.append('.')
-
-    return adj_cubes
-
-def create_pd(z_layer_0):
+def create_pd(z_layer_0, dimensions):
     pd = {}
     for y, col in enumerate(z_layer_0):
         for x, cube in enumerate(col):
-            pd[(x - 1, y - 1, 0)] = cube
+            pd[tuple([x - 1, y - 1] + [0] * (dimensions - 2))] = cube
 
     return pd
 
 def extend_pd(pd, axis_ranges):
-    for i in range(len(axis_ranges)):
-        axis_ranges[i][0] -= 1
-        axis_ranges[i][1] += 1
+    upd_axis_ranges = []
+    for axis in axis_ranges:
+        upd_axis_ranges.append([axis[0] - 1, axis[1] + 1])
 
-    for x in range(*axis_ranges[0]):
-        for y in range(*axis_ranges[1]):
-            for z in range(*axis_ranges[2]):
-                if pd.get((x, y, z)) is None:
-                    pd[(x, y, z)] = '.'
+    for x in range(*upd_axis_ranges[0]):
+        for y in range(*upd_axis_ranges[1]):
+            for z in range(*upd_axis_ranges[2]):
+                if len(upd_axis_ranges) == 4:
+                    for w in range(*upd_axis_ranges[3]):
+                        if pd.get((x, y, z, w)) is None:
+                            pd[(x, y, z, w)] = '.'
+                else:
+                    if pd.get((x, y, z)) is None:
+                        pd[(x, y, z)] = '.'
 
-    return pd, axis_ranges
+    return pd, upd_axis_ranges
 
-@timed
-def part_one(z_layer_0):
-    pd = create_pd(z_layer_0)
-    # The range has to be
-    axis_ranges = [[-1, len(z_layer_0[0]) - 1], [-1, len(z_layer_0) - 1], [0, 1]]
-    for _ in range(6):
+def get_adjacent_cubes(cycle_layout, mutable_layout, coords, dimensions):
+    adjustments = [adj for adj in itertools.product([-1, 0, 1], repeat=dimensions) if adj != tuple([0] * dimensions)]
+
+    adj_cubes = []
+    for adj in adjustments:
+        upd_coords = []
+        for axis, axis_adj in zip(coords, adj):
+            upd_coords.append(axis + axis_adj)
+        try:
+            adj_cubes.append(cycle_layout[tuple(upd_coords)])
+        except KeyError:
+            mutable_layout[tuple(upd_coords)] = '.'
+            adj_cubes.append('.')
+
+    return adj_cubes
+
+def cycle_pocket_dimension(z_layer_0, num_cycles, num_dimensions):
+    pd = create_pd(z_layer_0, num_dimensions)
+    axis_ranges = [[-1, len(z_layer_0) - 1], [-1, len(z_layer_0) - 1]] + [[0, 1]] * (num_dimensions - 2)
+    for _ in range(num_cycles):
         pd, axis_ranges = extend_pd(pd, axis_ranges)
         cycle_layout = pd.copy()
         for coords, cube_state in cycle_layout.items():
-            adj_cubes = get_adjacent_cubes(cycle_layout, pd, *coords)
+            adj_cubes = get_adjacent_cubes(cycle_layout, pd, coords, num_dimensions)
+
             if cube_state == '#' and adj_cubes.count('#') not in (2, 3):
                 pd[coords] = '.'
             elif cube_state == '.' and adj_cubes.count('#') == 3:
@@ -57,5 +63,14 @@ def part_one(z_layer_0):
 
     return list(pd.values()).count('#')
 
+@timed
+def part_one(z_layer_0):
+    return cycle_pocket_dimension(z_layer_0, 6, 3)
+
+@timed
+def part_two(z_layer_0):
+    return cycle_pocket_dimension(z_layer_0, 6, 4)
+
 
 print(part_one(z_layer_0))
+print(part_two(z_layer_0))
