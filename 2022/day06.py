3187d5d4029c6fd7a2a1ca65e4272d26b0dc234c
SharpBit <31069084+SharpBit@users.noreply.github.com>
2022-12-06 21:08:01 -0500

    day 6 but not overcomplicated

--- 2022/day06.py
+++ 2022/day06.py
@@ -1,5 +1,3 @@
-from collections import deque
-
 from utils import timed
 
 
@@ -7,23 +5,15 @@ stream = open('inputs/day06.txt').read().strip()
 
 @timed
 def part_one():
-    marker = deque()
-    for i, char in enumerate(stream):
-        marker.append(char)
-        if len(marker) > 4:
-            marker.popleft()
-        if len(set(marker)) == 4:
-            return i + 1
+    for i in range(len(stream)):
+        if len(set(stream[i:i + 4])) == 4:
+            return i + 4
 
 @timed
 def part_two():
-    marker = deque()
-    for i, char in enumerate(stream):
-        marker.append(char)
-        if len(marker) > 14:
-            marker.popleft()
-        if len(set(marker)) == 14:
-            return i + 1
+    for i in range(len(stream)):
+        if len(set(stream[i:i + 14])) == 14:
+            return i + 14
 
 
 print(part_one())
