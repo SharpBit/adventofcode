e8f434f56fa5278ad31c30bcecd2b4346ac6baa1
SharpBit <31069084+SharpBit@users.noreply.github.com>
2025-12-02 11:23:33 -0500

    refactor day 1 to use foldl'

--- 2025/day01.hs
+++ 2025/day01.hs
@@ -1,45 +1,38 @@
 import Utils (readLines)
-import Debug.Trace
+import Data.List (foldl')
 
--- I'll refactor this with foldl' later
+step1 :: (Int, Int) -> String -> (Int, Int)
+step1 (pointer, count) rotation =
+    case rotation of
+        (dir:amt) ->
+            let
+                num = read amt :: Int
+                op = if dir == 'L' then (-) else (+) :: Int -> Int -> Int
+                newPointer = (pointer `op` num) `mod` 100
+                count' = if newPointer == 0 then count + 1 else count
+            in (newPointer, count')
+        _ -> (pointer, count)
 
-parsePassword :: [String] -> Int -> Int -> Int
-parsePassword rotations i pointer
-    | i >= (length rotations) = 0
-    | null rotation = parsePassword rotations (i + 1) pointer  -- shouldn't happen
-    | otherwise =
-        let
-            dir = head rotation
-            num = read (tail rotation) :: Int
-            op = if dir == 'L' then (-) else (+) :: Int -> Int -> Int
-            newPointer = (pointer `op` num) `mod` 100
-            rest = parsePassword rotations (i + 1) newPointer
-            add = if newPointer == 0 then 1 else 0
-        in (add + rest)
-    where rotation = rotations !! i
-
-parsePassword2 :: [String] -> Int -> Int -> Int
-parsePassword2 rotations i pointer
-    | i >= (length rotations) = 0
-    | null rotation = parsePassword2 rotations (i + 1) pointer  -- shouldn't happen
-    | otherwise =
-        let
-            dir = head rotation
-            num = read (tail rotation) :: Int
-            op = if dir == 'L' then (-) else (+) :: Int -> Int -> Int
-            newPointer = (pointer `op` num)
-            rest = parsePassword2 rotations (i + 1) (newPointer `mod` 100)
-            add | newPointer > 0 = newPointer `div` 100
-                | pointer == 0 && newPointer <= 0 = (abs newPointer) `div` 100
-                | otherwise = (abs newPointer) `div` 100 + 1
-        in (add + rest)
-    where rotation = rotations !! i
+step2 :: (Int, Int) -> String -> (Int, Int)
+step2 (pointer, count) rotation =
+    case rotation of
+        (dir:amt) ->
+            let
+                num = read amt :: Int
+                op = if dir == 'L' then (-) else (+) :: Int -> Int -> Int
+                newPointer = (pointer `op` num)
+                add | newPointer > 0 = newPointer `div` 100
+                    | pointer == 0 && newPointer <= 0 = (abs newPointer) `div` 100
+                    | otherwise = (abs newPointer) `div` 100 + 1
+                count' = count + add
+            in (newPointer `mod` 100, count')
+        _ -> (pointer, count)
 
 part1 :: [String] -> Int
-part1 rotations = parsePassword rotations 0 50
+part1 rotations = snd (foldl' step1 (50, 0) rotations)
 
 part2 :: [String] -> Int
-part2 rotations = parsePassword2 rotations 0 50
+part2 rotations = snd (foldl' step2 (50, 0) rotations)
 
 main :: IO ()
 main = do
