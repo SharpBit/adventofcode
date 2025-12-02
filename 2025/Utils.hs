75ed08ee375b224cd8b3c57c1f0fa70ca49ab44b
SharpBit <31069084+SharpBit@users.noreply.github.com>
2025-12-02 17:53:32 -0500

    splitStr, strip, and pair

--- 2025/Utils.hs
+++ 2025/Utils.hs
@@ -1,11 +1,18 @@
 module Utils (
     readLines,
     readWords,
-    slice
+    slice,
+    splitStr,
+    strip,
+    pair
 ) where
 
 import System.IO
 import Control.Monad
+import Data.List (dropWhile, dropWhileEnd)
+
+whitespace :: [Char]
+whitespace = [' ', '\t', '\n']
 
 readLines :: String -> IO [String]
 readLines fname = fmap lines (readFile fname)
@@ -15,3 +22,15 @@ readWords fname = fmap words (readFile fname)
 
 slice :: Int -> Int -> String -> String
 slice start end str = take (end - start) (drop start str)
+
+splitStr :: Char -> String -> [String]
+splitStr delim str = case break (== delim) str of
+    (a, "") -> [a]
+    (a, _ : b) -> a : splitStr delim b
+
+-- Strip whitespace from the beginning and end of str
+strip :: String -> String
+strip str = dropWhileEnd (`elem` whitespace) (dropWhile (`elem` whitespace) str)
+
+pair :: [a] -> (a, a)
+pair [x, y] = (x, y)
