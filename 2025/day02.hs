ae9bfc1ff05a18c13a37fc06cce3cec93b89561a
SharpBit <31069084+SharpBit@users.noreply.github.com>
2025-12-02 20:45:14 -0500

    day 2 part 2

--- 2025/day02.hs
+++ 2025/day02.hs
@@ -1,21 +1,34 @@
 import Utils (splitStr, pair, strip, slice)
+import Data.List (nub)
 
-invalidID :: Int -> Bool
-invalidID id =
-    let
-        idStr = show id
-        mid = length idStr `div` 2
-        first = slice 0 mid idStr
-        second = slice mid (length idStr) idStr
-    in first == second
+splitStrEqually :: Int -> String -> [String]
+splitStrEqually n str
+    | n <= 0 = []
+    | otherwise =
+        let sep = length str `div` n
+        in slice 0 sep str : splitStrEqually (n - 1) (slice sep (length str) str)
 
-step :: Int -> (Int, Int) -> Int
-step res (start, end) =
-    let invalidIDs = [id | id <- [start..end + 1], invalidID id]
+-- Check if an ID is invalid, when split into n..max pieces
+invalidID :: Int -> Int -> String -> Bool
+invalidID n max id
+    | n > max = False
+    | otherwise = (length id `mod` n == 0 && length (nub (splitStrEqually n id)) == 1) || invalidID (n + 1) max id
+
+step1 :: Int -> (Int, Int) -> Int
+step1 res (start, end) =
+    let invalidIDs = [id | id <- [start..end + 1], invalidID 2 2 (show id)]
+    in res + sum invalidIDs
+
+step2 :: Int -> (Int, Int) -> Int
+step2 res (start, end) =
+    let invalidIDs = [id | id <- [start..end + 1], invalidID 2 (length (show id)) (show id)]
     in res + sum invalidIDs
 
 part1 :: [(Int, Int)] -> Int
-part1 productRanges = foldl' step 0 productRanges
+part1 productRanges = foldl' step1 0 productRanges
+
+part2 :: [(Int, Int)] -> Int
+part2 productRanges = foldl' step2 0 productRanges
 
 main :: IO ()
 main = do
@@ -23,3 +36,4 @@ main = do
     let productRanges' = fmap pair (fmap (splitStr '-') (fmap strip (splitStr ',' input)))
         productRanges = map (\(a, b) -> (read a :: Int, read b :: Int)) productRanges'
     print (part1 productRanges)
+    print (part2 productRanges)
