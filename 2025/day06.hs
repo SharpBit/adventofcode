99ff9f8d69eb5b4ddff276ba28721243267ff8a0
SharpBit <31069084+SharpBit@users.noreply.github.com>
2025-12-07 13:20:47 -0500

    day 6 part 2

--- 2025/day06.hs
+++ 2025/day06.hs
@@ -1,10 +1,32 @@
 import Utils (readLines)
-import Data.List (transpose, foldl1)
+import Data.List (transpose, foldl1, foldl')
+
+parseNumber :: String -> Int -> Int -> [String] -> Int
+parseNumber curr i j input
+    | j >= length input - 1 = read curr :: Int
+    | i < 0 || i >= length (head input) = 0
+    | otherwise =
+        if input !! j !! i /= ' '
+        then parseNumber (curr ++ [(input !! j !! i)]) i (j + 1)  input
+        else parseNumber curr i (j + 1) input
 
 part1 :: [[Int]] -> [Int -> Int -> Int] -> Int
 part1 nums ops =
     let cols = transpose nums
     in sum (zipWith (\op col -> foldl1 op col) ops cols)
+
+part2 :: Int -> Int -> [Int] -> [String] -> Int
+part2 res i curr input
+    | i < 0 = res
+    | otherwise =
+        let
+            num = parseNumber "" i 0 input
+            curr' = num : curr
+            op = input !! (length input - 1) !! i
+        in case op of
+            '+' -> part2 (res + (foldl' (+) 0 curr')) (i - 2) [] input
+            '*' -> part2 (res + (foldl' (*) 1 curr')) (i - 2) [] input
+            _ -> part2 res (i - 1) curr' input
 
 main :: IO ()
 main = do
@@ -13,4 +35,4 @@ main = do
     let ops :: [Int -> Int -> Int]
         ops = map (\op -> if op == "+" then (+) else (*)) (words (last input))
     print (part1 nums ops)
-    -- I do not feel like doing part 2 in haskell, maybe i'll do it in python
+    print (part2 0 (length (head input) - 1) [] input)
